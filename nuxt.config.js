5bf274087f72e447967e4e8f2d79fdbfeeb0daf0
conoha <xxx>
2021-04-28 14:57:24 +0900

    plugin add

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     { src: '~/plugins/swiper', mode: 'client' },
-    { src: "~/plugins/fb.js", mode: 'client' },
+    { src: '~/plugins/vue-fb-customer-chat.js', ssr: false }
   ],
 
 
