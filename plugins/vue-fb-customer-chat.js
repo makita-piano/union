a05552b87fe6be7a3031752ec160e04b5e2f6e71
conoha <xxx>
2021-04-28 19:37:55 +0900

    japanese messanger

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -4,5 +4,5 @@ import VueFbCustomerChat from 'vue-fb-customer-chat'
 Vue.use(VueFbCustomerChat, {
   page_id: '103874421592721', //  change 'null' to your Facebook Page ID,
   theme_color: '#333333', // theme color in HEX
-  locale: 'en_US', // default 'en_US'
+  locale: 'ja_JP', // default 'en_US'
 })
