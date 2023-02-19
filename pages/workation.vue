82c233371f7b094cc6748351c460d1924d306992
makki! <41937003+makita-piano@users.noreply.github.com>
2023-02-19 21:31:38 +0900

    printer img add

--- pages/workation.vue
+++ pages/workation.vue
@@ -55,19 +55,99 @@
                 </tr>
               </tbody>
             </table>
+            <div class="grid-center col-12" style="background: rgb(238, 238, 238 , 0.5);padding-top: 1rem">
+              <div class="grid col-12" style="max-width: 768px;padding: 0 1rem 2rem;">
+                <div class="col-6_sm-12">
+                  <nuxt-img src="/cycle1.jpg" width="100%" style="object-fit: cover;padding: .5rem 2rem " alt="top" />
+                </div>
+                <div class="col-6_sm-12">
+                  <nuxt-img src="/cycle2.jpg" width="100%" style="object-fit: cover;padding: .5rem 2rem" alt="top" />
+                </div>
+              </div>
+            </div>
+            <h3 class="col-12" style="margin-bottom: 1rem">共用パソコン</h3>
+            <table style="padding: 0 2rem;border-collapse: collapse;width: 100%">
+              <tbody>
+                <tr>
+                  <td colspan="1" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    料金
+                  </td>
+                  <td colspan="3" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    ゲストの方は2時間まで無料/一般のお客様は1時間¥300
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="4" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    iMac M1
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="grid-center col-12" style="background: rgb(238, 238, 238 , 0.5);padding-top: 1rem">
+            <div class="grid-center col-12" style="max-width: 768px;padding: 0 1rem 2rem;">
+              <div class="col-6_sm-12">
+                <nuxt-img src="/workation/imac.jpg" width="100%" style="object-fit: cover;padding: .5rem 2rem " alt="top" />
+              </div>
+            </div>
+          </div>
+          <div class="grid col-12_sm-12" style="margin: auto;max-width: 768px;margin-bottom: 1rem">
+            <h3 class="col-12" style="margin-bottom: 1rem">A3対応複合機/h3>
+            <table style="padding: 0 2rem;border-collapse: collapse;width: 100%">
+              <tbody>
+                <tr>
+                  <td colspan="1" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    モノクロ
+                  </td>
+                  <td colspan="1" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    ¥10
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="1" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    カラー
+                  </td>
+                  <td colspan="1" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    ¥50
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="2" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    写真など印刷されたい方はご相談くださいませ。
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="grid col-12_sm-12" style="margin: auto;max-width: 768px;margin-bottom: 1rem">
+            <h3 class="col-12" style="margin-bottom: 1rem">フォトプリンタ</h3>
+            <table style="padding: 0 2rem;border-collapse: collapse;width: 100%">
+              <tbody>
+                <tr>
+                  <td colspan="1" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    フォト用紙シールタイプ
+                  </td>
+                  <td colspan="1" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    ¥600/10枚セット
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="2" style="border: 1px solid #000;padding: 1rem;border-collapse: collapse;width: auto">
+                    プリンタの貸出は無料です
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="grid-center col-12" style="background: rgb(238, 238, 238 , 0.5);padding-top: 1rem">
+            <div class="grid-center col-12" style="max-width: 768px;padding: 0 1rem 2rem;">
+              <div class="col-6_sm-12">
+                <nuxt-img src="/workation/printer.jpg" width="100%" style="object-fit: cover;padding: .5rem 2rem " alt="top" />
+              </div>
+            </div>
           </div>
         </div>
       </div>    
-      <div class="grid-center col-12" style="background: rgb(238, 238, 238 , 0.5);padding-top: 1rem">
-        <div class="grid col-12" style="max-width: 768px;padding: 0 1rem 2rem;">
-          <div class="col-6_sm-12">
-            <nuxt-img src="/cycle1.jpg" width="100%" style="object-fit: cover;padding: .5rem 2rem " alt="top" />
-          </div>
-          <div class="col-6_sm-12">
-            <nuxt-img src="/cycle2.jpg" width="100%" style="object-fit: cover;padding: .5rem 2rem" alt="top" />
-          </div>
-        </div>
-      </div>
       <div class="grid-center col-12" style="background: rgb(238, 238, 238 , 0.5);padding-top: 1rem">
         <div class="col-12" style="margin-bottom: 2rem">
           <h2 style="text-align: center;margin-top: 2rem">CYCLE MAP</h2>
